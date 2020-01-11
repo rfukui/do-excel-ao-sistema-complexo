406a4c24a848bfd4b7a3c6c4da30ef625c91a811
Ricardo Fukui <ricardo.fukui@escale.com.br>
2020-01-11 00:40:16 -0300

    WIP

---
+++ 03-usando-mvc/new_store/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  "extends": "airbnb-base",
+  "rules": {
+    "no-console": 0,
+    "no-param-reassign": [2, {"props": false}],
+    "prefer-destructuring": 0,
+    "treatUndefinedAsUnspecified": true,
+    "arrow-body-style": 0,
+    "comma-dangle": 0,
+  },
+  "env": {
+    "commonjs": true,
+    "node": true,
+    "mocha": true
+  },
+}
---
+++ 03-usando-mvc/new_store/api/server/src/config/config.js
@@ -0,0 +1,26 @@
+{
+  "development": {
+    "username": "postgres",
+    "password": null,
+    "database": "new_store_development",
+    "host": "127.0.0.1",
+    "dialect": "postgres",
+    "operatorsAliases": false
+  },
+  "test": {
+    "username": "postgres",
+    "password": null,
+    "database": "new_store_test",
+    "host": "127.0.0.1",
+    "dialect": "postgres",
+    "operatorsAliases": false
+  },
+  "production": {
+    "username": "postgres",
+    "password": null,
+    "database": "new_store_production",
+    "host": "127.0.0.1",
+    "dialect": "postgres",
+    "operatorsAliases": false
+  }
+}
